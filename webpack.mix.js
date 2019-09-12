b5ebb73cdf93f7b7d1d7643782042ff1bb2eff83
David Peralta <peraltadavidr@outlok.com>
2019-09-12 12:58:34 +0800

    Remove alias

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,13 +10,6 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-mix.webpackConfig({
-resolve: {
-    alias: {
-    "@" : path.resolve(__dirname, '/resources/')
-    }
-}
-})
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');

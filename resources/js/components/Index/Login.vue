259f8ce5c6b2c50284c689ac2b893f5694a8c232
David Peralta <peraltadavidr@outlok.com>
2019-09-12 12:41:58 +0800

    Create index page and components

---
+++ resources/js/components/Index/Login.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="login-component">
+        
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    
+</style>

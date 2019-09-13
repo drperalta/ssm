f1c04f2212cd44d1a82fdee71c41eabac8aa2456
David Peralta <peraltadavidr@outlok.com>
2019-09-13 21:25:08 +0800

    Configure laravel passport

--- routes/api.php
+++ routes/api.php
@@ -13,11 +13,11 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::group([ 'prefix' => 'auth'], function() {
     Route::post('login','AuthController@login');
     Route::post('register','AuthController@register');
+});
+
+Route::middleware('auth:api')->group(function () {
+    // 
 });

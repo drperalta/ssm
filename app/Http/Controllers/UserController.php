9d90ff5e5df3df3d1c58ec258df0990319fd0041
David Peralta <peraltadavidr@outlok.com>
2019-09-14 02:08:37 +0800

    Limit user information

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,6 @@ class UserController extends Controller
 {
     public function user()
     {
-        return response()->json(['user' => auth()->user()], 200);
+        return response()->json(['user' => auth()->user()->first(['fullname','username','email'])], 200);
     }
 }
